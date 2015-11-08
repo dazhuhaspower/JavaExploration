a6b46ea3c0a1e9bf08a9d880a642cd30792485d4
dazhuhaspower <jiefenghaspower@gmail.com>
2015-11-08 12:34:06 -0800

    update

--- src/java8Features/LambdaExpressionExplr.java
+++ src/java8Features/LambdaExpressionExplr.java
@@ -1,9 +1,19 @@
 package java8Features;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -11,11 +21,14 @@ import java.util.function.Supplier;
 
 public class LambdaExpressionExplr {
     static List<String> list = Arrays.asList("VMware", "RSA", "Pivotal", "EMC", "Dell");
+    static Consumer<Object> consumer = System.out::println;
 
     public static void main(String[] args) {
         runningTask1();
         runningTask2();
         runningTask3();
+        runningTask4();
+        runningTask5();
     }
 
     public static void runningTask1() {
@@ -89,22 +102,92 @@ public class LambdaExpressionExplr {
         Supplier<Integer> supplier = () -> (int) (Math.random() * 10);
         System.out.println(supplier.get());
 
-        Consumer<Boolean> boolConsumer = (x) -> System.out.println(x);
-        Consumer<Integer> intConsumer = (x) -> System.out.println(x);
-        intConsumer.accept(supplier.get());
+        Consumer<Object> consumer = System.out::println;
+        consumer.accept(supplier.get());
 
         Comparator<String> comparator = (x, y) -> x.length()-y.length();
-        boolConsumer.accept(comparator.compare("EMC", "VMware") < 0);
-        boolConsumer.accept(comparator.reversed().compare("EMC", "VMware") < 0);
+        consumer.accept(comparator.compare("EMC", "VMware") < 0);
+        consumer.accept(comparator.reversed().compare("EMC", "VMware") < 0);
     }
 
     //streams
     public static void runningTask3() {
         Comparator<String> comparator = (x, y) -> x.length()-y.length();
+        Predicate<String> predicate = (s) -> s.toLowerCase().indexOf('a') > 0;
+
+        //sequential
         list.stream()
             .sorted(comparator)
             .map(String::toUpperCase)
-            .filter((s) -> s.toLowerCase().indexOf('a') > 0 )
+            .filter(predicate)
             .forEach(System.out::println);
+
+        boolean anyContainsA = list.stream().anyMatch(predicate);
+        consumer.accept(anyContainsA);
+        boolean allContainsA = list.stream().allMatch(predicate);
+        consumer.accept(allContainsA);
+        boolean noneContainsA = list.stream().noneMatch(predicate);
+        consumer.accept(noneContainsA);
+        long countAnyContainsA = list.stream().filter(predicate).count();
+        consumer.accept(countAnyContainsA);
+        String reduced = list.stream().filter(predicate).reduce("", (a, b) -> a + "." + b);
+        consumer.accept(reduced);
+        Optional<String> reduced2 = list.stream().filter(predicate).reduce((a, b) -> a + "." + b);
+        reduced2.ifPresent(consumer);
+
+        //parallel
+        long slowSortedCount = list.stream().sorted().count();
+        long fastSortedCount = list.parallelStream().sorted().count();
+        consumer.accept(slowSortedCount);
+        consumer.accept(fastSortedCount);
+    }
+
+    //map
+    public static void runningTask4() {
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        for(int i=0; i<list.size(); i++) {
+            map.putIfAbsent(i, list.get(i));
+        }
+        map.forEach((key, val) -> System.out.println(val));
+        map.computeIfPresent(0, (k, v) -> "Dazhu");
+        consumer.accept(map);
+        map.computeIfPresent(1, (k, v) -> null);
+        consumer.accept(map);
+        map.computeIfAbsent(1, k -> "EMC");
+        consumer.accept(map);
+        map.computeIfAbsent(1, k -> "haspower");
+        consumer.accept(map);
+        map.remove(1, "Dazhu");
+        consumer.accept(map);
+        map.remove(0, "Dazhu");
+        consumer.accept(map);
+        consumer.accept(map.getOrDefault(0, "Dazhu has power"));
+        map.merge(0, "Dazhu", (val, newVal) -> val.concat(newVal));
+        consumer.accept(map);
+        map.merge(0, "haspower", (val, newVal) -> val.concat(newVal));
+        consumer.accept(map);
+    }
+
+    //date
+    public static void runningTask5() {
+        Clock clock = Clock.systemDefaultZone();
+        long millis = clock.millis();
+        Instant instant = clock.instant();
+        Date legacyDate = Date.from(instant);
+
+        Set<String> zones = ZoneId.getAvailableZoneIds();
+        zones.stream().filter((x) -> x.toLowerCase().contains("pacific")).forEach(System.out::println);
+        zones.stream().filter((x) -> x.toLowerCase().contains("8")).forEach(System.out::println);
+        ZoneId zone1 = ZoneId.of("US/Pacific");
+        ZoneId zone2 = ZoneId.of("Etc/GMT-8");
+        consumer.accept(zone1.getRules());
+        consumer.accept(zone2.getRules());
+
+        LocalTime localTime1 = LocalTime.now(zone1);
+        consumer.accept(localTime1);
+        LocalTime localTime2 = LocalTime.now(zone2);
+        consumer.accept(localTime2);
+        consumer.accept(localTime1.isAfter(localTime2));
+        consumer.accept(ChronoUnit.HOURS.between(localTime1, localTime2));
     }
 }
